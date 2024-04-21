9ecac8f9cd46cd64ca8e62ed364b6fb946571c5f
Krzysztof Klejbor <krzysztof.klejbor@gmail.com>
2024-04-21 17:19:03 +0200

    Add comment explaining the fucking problem

--- code/firefly-algorithm/src/main/java/edu/umg/App.java
+++ code/firefly-algorithm/src/main/java/edu/umg/App.java
@@ -5,11 +5,13 @@ import edu.umg.helpers.benchmark_functions.multidimensional.Rastrigin;
 
 import java.util.Arrays;
 
-/**
- * Hello world!
- */
 public class App {
     public static void main(String[] args) {
+        /*
+         * What the fuck!? For some reason this version performs worse than the version with objects!?
+         * What kind of black fucking magic had Jan Olszewski and Ewa Figielska used in their version of the algorithm!?
+         * Fuck this. Do not try to improve this algorithm!!!
+         */
         for (int i = 0; i < 10; i++) {
             Rastrigin Rastrigin = new Rastrigin();
             FireflyAlgorithm fireflyAlgorithm = new FireflyAlgorithm(1, 0.1, 0.8, 0.98, 90, 2000, Rastrigin, 4, -5.12, 5.12, true);
