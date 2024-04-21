641b6f922f87321ee6ad3113717b2c222a7b75e8
Krzysztof Klejbor <krzysztof.klejbor@gmail.com>
2024-04-21 14:58:35 +0200

    Fix imports and change variables

--- code/firefly-algorithm/src/main/java/edu/umg/App.java
+++ code/firefly-algorithm/src/main/java/edu/umg/App.java
@@ -1,7 +1,7 @@
 package edu.umg;
 
 import edu.umg.algorithms.synchronous.FireflyAlgorithm;
-import edu.umg.helpers.RastriginFunction;
+import edu.umg.helpers.benchmark_functions.multidimensional.Rastrigin;
 import edu.umg.algorithms.synchronous.objects.Firefly;
 
 /**
@@ -10,9 +10,9 @@ import edu.umg.algorithms.synchronous.objects.Firefly;
 public class App {
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
-            FireflyAlgorithm fireflyAlgorithm = new FireflyAlgorithm(1, 0.1, 0.8, 0.98, 90, 1000, new RastriginFunction(), 4, -5.12, 5.12, true);
+            FireflyAlgorithm fireflyAlgorithm = new FireflyAlgorithm(1, 0.1, 0.8, 0.98, 90, 2000, new Rastrigin(), 4, -5.12, 5.12, true);
 
-            Firefly finalSolution = new Firefly(fireflyAlgorithm.run(), new RastriginFunction(), false);
+            Firefly finalSolution = new Firefly(fireflyAlgorithm.run(), new Rastrigin(), false);
 
             System.out.printf("Najlepsze rozwiązanie: \n%s\n\n", finalSolution);
         }
