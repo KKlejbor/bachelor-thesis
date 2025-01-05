61474800b74d84b69e0ecc14ff5efa4265178038
Krzysztof Klejbor <krzysztof.klejbor@gmail.com>
2025-01-05 01:31:58 +0100

    Fuck Java!
    All my homies use C#

---
+++ code/firefly-algorithm-v2-electric-boogaloo/firefly-algorithm-v2-electric-boogaloo/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace firefly_algorithm_v2_electric_boogaloo;
+
+public abstract class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello world!");
+    }
+}
